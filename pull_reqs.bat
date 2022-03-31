870024ef1ecc2c53ae1018f02806207279a6c539
Robin Adili <robin.adili@inria.fr>
2022-03-31 13:41:22 +0200

    install patch with dependencies + change version to 2020.1.post2

--- pull_reqs.bat
+++ pull_reqs.bat
@@ -12,7 +12,6 @@ for /F "tokens=1,2 delims=;" %%i in (reqs_win.txt) do (
         "%BUILDDIR%\%%i.exe" /S /D=%BUILDDIR%\%%i
 )
 
-curl.exe -C - -H "User-Agent: Mozilla/5.0 (X11; Linux x86_64; rv:79.0) Gecko/20100101 Firefox/79.0" -L -o "%BUILDDIR%\patch.py" "https://raw.githubusercontent.com/techtonik/python-patch/master/patch.py"
-python "%BUILDDIR%\patch.py" --verbose --directory "%CURRENTDIR%" --debug patch 
+python -m patch --verbose --directory "%CURRENTDIR%" patch 
 move "%BUILDDIR%\fbxsdk\lib\vs2017" "%BUILDDIR%\fbxsdk\lib\all"
 popd

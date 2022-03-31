7fa4cfb131e0d4a63b355d44ccf345c3fd5b76f9
Robin Adili <robin.adili@inria.fr>
2022-03-31 13:41:22 +0200

    fix curl + patch

--- pull_reqs.bat
+++ pull_reqs.bat
@@ -8,12 +8,11 @@ mkdir "%FBXSDKDIR%"
 mkdir "%FBXSDKPYTHONDIR%"
 
 for /F "tokens=1,2 delims=;" %%i in (reqs_win.txt) do (
-        curl -H 'User-Agent: Mozilla/5.0 (X11; Linux x86_64; rv:79.0) Gecko/20100101 Firefox/79.0' -L -o "%BUILDDIR%\%%i.exe" %%j
+        curl.exe -C - -H "User-Agent: Mozilla/5.0 (X11; Linux x86_64; rv:79.0) Gecko/20100101 Firefox/79.0" -L -o "%BUILDDIR%\%%i.exe" %%j
         "%BUILDDIR%\%%i.exe" /S /D=%BUILDDIR%\%%i
 )
 
-curl -H 'User-Agent: Mozilla/5.0 (X11; Linux x86_64; rv:79.0) Gecko/20100101 Firefox/79.0' -L -o "%BUILDDIR%\patch.py" "https://raw.githubusercontent.com/techtonik/python-patch/master/patch.py"
-python "%BUILDDIR%\patch.py" patch
-
-move "%BUILDDIR%\fbxsdk\lib\vs2017" "%BUILDDIR%\fbxsdk\lib\all" >nul
+curl.exe -C - -H "User-Agent: Mozilla/5.0 (X11; Linux x86_64; rv:79.0) Gecko/20100101 Firefox/79.0" -L -o "%BUILDDIR%\patch.py" "https://raw.githubusercontent.com/techtonik/python-patch/master/patch.py"
+python "%BUILDDIR%\patch.py" --verbose --directory "%CURRENTDIR%" --debug patch 
+move "%BUILDDIR%\fbxsdk\lib\vs2017" "%BUILDDIR%\fbxsdk\lib\all"
 popd
